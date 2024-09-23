e81eb563e70f91e329e5637fd6846b814b5b6d25
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-09-23 10:28:22 -0400

    Figure out the plan for implementation

--- TurtleRace.py
+++ TurtleRace.py
@@ -2,3 +2,17 @@
 Version 1
 In this version the turtle race has a flat single-file structure
 """
+
+#Create the race track window and configure it.
+
+#Create the racers
+
+#Position the racers on the starting line
+
+#Run the race
+
+#Determine the winner
+
+#Let the user know who the winner is
+
+#Wait for user confirmation to close the program
