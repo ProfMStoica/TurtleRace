b2bf4525b02a023a661a5f2d3b55be05016f6a7a
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-09-27 15:47:08 -0400

    Change the names of the racer variables to ninja turtles

--- TurtleRace.py
+++ TurtleRace.py
@@ -11,53 +11,53 @@ raceTrack = turtle.Screen()
 raceTrack.bgcolor('lightgreen')
 
 #Create the racers
-larry = turtle.Turtle()
-larry.shape("turtle")
-larry.color("darkgreen")
-larry.turtlesize(3, 3, 1)
-larry.penup()
+leo = turtle.Turtle()
+leo.shape("turtle")
+leo.color("darkgreen")
+leo.turtlesize(3, 3, 1)
+leo.penup()
 
-curly = turtle.Turtle()
-curly.shape("turtle")
-curly.color("darkred")
-curly.turtlesize(3, 3, 1)
-curly.penup()
+mikey = turtle.Turtle()
+mikey.shape("turtle")
+mikey.color("darkred")
+mikey.turtlesize(3, 3, 1)
+mikey.penup()
 
-moe = turtle.Turtle()
-moe.shape("turtle")
-moe.color("blue")
-moe.turtlesize(3, 3, 1)
-moe.penup()
+don = turtle.Turtle()
+don.shape("turtle")
+don.color("blue")
+don.turtlesize(3, 3, 1)
+don.penup()
 
 #Position the racers on the starting line
-larry.left(180)
-larry.forward(200)
-larry.right(90)
-curly.left(90)
-moe.forward(200)
-moe.left(90)
+leo.left(180)
+leo.forward(200)
+leo.right(90)
+mikey.left(90)
+don.forward(200)
+don.left(90)
 
 #get them ready to race
-larry.pendown()
-curly.pendown()
-moe.pendown()
+leo.pendown()
+mikey.pendown()
+don.pendown()
 
 #Run the race
 dist = random.randint(10, 100)
-larry.forward(dist)
+leo.forward(dist)
 
 dist = random.randint(10, 100)
-curly.forward((dist))
+mikey.forward((dist))
 
 dist = random.randint(10, 100)
-moe.forward(dist)
+don.forward(dist)
 
 #Determine the winner
 #TODO: use branching to determine which turtle advanced the most
-winner = moe
+winner = don
 
 #Let the user know who the winner is
-messagebox.showinfo("Turtle Race", "Moe")
+messagebox.showinfo("Turtle Race", "Mikey")
 
 #Wait for user confirmation to close the program
 raceTrack.exitonclick()
