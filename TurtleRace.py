a5bc168d33c487f35010ba8985a4417b7bdda25d
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-09-27 17:37:41 -0400

    Restructure the code to use functions.

--- TurtleRace.py
+++ TurtleRace.py
@@ -7,54 +7,72 @@ import random
 from tkinter import messagebox
 
 #Create the race track window and configure it.
-raceTrack = turtle.Screen()
-raceTrack.bgcolor('lightgreen')
+def createRaceTrack():
+    raceTrack = turtle.Screen()
+    raceTrack.bgcolor('lightgreen')
+    return raceTrack
 
 #Create the racers
-leo = turtle.Turtle()
-leo.shape("turtle")
-leo.color("darkgreen")
-leo.turtlesize(3, 3, 1)
-leo.penup()
+def registerRacers():
+    leo = turtle.Turtle()
+    leo.shape("turtle")
+    leo.color("darkgreen")
+    leo.turtlesize(3, 3, 1)
+    leo.penup()
 
-mikey = turtle.Turtle()
-mikey.shape("turtle")
-mikey.color("darkred")
-mikey.turtlesize(3, 3, 1)
-mikey.penup()
+    mikey = turtle.Turtle()
+    mikey.shape("turtle")
+    mikey.color("darkred")
+    mikey.turtlesize(3, 3, 1)
+    mikey.penup()
 
-don = turtle.Turtle()
-don.shape("turtle")
-don.color("blue")
-don.turtlesize(3, 3, 1)
-don.penup()
+    don = turtle.Turtle()
+    don.shape("turtle")
+    don.color("blue")
+    don.turtlesize(3, 3, 1)
+    don.penup()
+    return leo,mikey,don
 
 #Position the racers on the starting line
-leo.left(180)
-leo.forward(200)
-leo.right(90)
-mikey.left(90)
-don.forward(200)
-don.left(90)
+def positionRacers(leo, mikey, don):
+    leo.left(180)
+    leo.forward(200)
+    leo.right(90)
+    mikey.left(90)
+    don.forward(200)
+    don.left(90)
 
 #get them ready to race
-leo.pendown()
-mikey.pendown()
-don.pendown()
+    leo.pendown()
+    mikey.pendown()
+    don.pendown()
 
 #Run the race
-dist = random.randint(10, 100)
-leo.forward(dist)
+def runRace(leo, mikey, don):
+    dist = random.randint(10, 100)
+    leo.forward(dist)
 
-dist = random.randint(10, 100)
-mikey.forward((dist))
+    dist = random.randint(10, 100)
+    mikey.forward((dist))
 
-dist = random.randint(10, 100)
-don.forward(dist)
+    dist = random.randint(10, 100)
+    don.forward(dist)
 
 #Determine the winner
-#TODO: use branching to determine which turtle advanced the most
-winner = don
+def determineWinner(don):
+    #TODO: use branching to determine which turtle advanced the most
+    winner = don
+
+#run the main program using the functions we have defined
+raceTrack = createRaceTrack()
+
+leo, mikey, don = registerRacers()
+
+positionRacers(leo, mikey, don)
+
+runRace(leo, mikey, don)
+
+determineWinner(don)
 
 #Let the user know who the winner is
 messagebox.showinfo("Turtle Race", "Mikey")
